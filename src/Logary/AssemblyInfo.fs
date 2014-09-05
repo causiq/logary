50dbf436ee4cb15cfbb0df9f812558bfecc6bc78
Henrik <henrik@haf.se>
2014-09-05 20:35:10 +0200

    Sign Logary by default with snk
    This snk file is not password protected.

--- src/Logary/AssemblyInfo.fs
+++ src/Logary/AssemblyInfo.fs
@@ -3,4 +3,5 @@ open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 [<assembly: InternalsVisibleTo("Intelliplan.Logary.Tests")>]
+[<assembly: StrongNameKeyPair("../signing/LogaryPublic.snk")>]
 ()
