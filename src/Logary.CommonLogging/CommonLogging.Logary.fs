62fab43be11bfc9bf167438db5f529ca12ee72a8
Henrik Feldt <henrik@haf.se>
2014-10-22 10:01:18 +0200

    stub for CommonLogging, ping @parliament718 - just finish interface implementation

---
+++ src/Logary.CommonLogging/CommonLogging.Logary.fs
@@ -0,0 +1,20 @@
+﻿module Impl
+
+open System
+
+open Common.Logging
+
+open Logary
+
+type internal Adapter(l : Logger) =
+  interface ILog with
+    // TODO: implement me
+    member x.IsTraceEnabled = l.Level >= Verbose
+    // etc
+
+type LogaryAdapter(lm : LogManager) =
+  interface ILoggerFactoryAdapter with
+    member x.GetLogger (typ : Type) =
+      Adapter(lm.GetLogger(typ.FullName)) :> ILog
+    member x.GetLogger (name : string) =
+      lm.GetLogger name
