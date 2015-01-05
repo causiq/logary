c6fccc317a845e7f7c11512b4be7756178104c19
Henrik Feldt <henrik@haf.se>
2015-01-05 13:18:46 +0100

    bump Suave, FluentMigrator, paket bootstrapper

--- src/Logary.Suave.Tests/AdapterTests.fs
+++ src/Logary.Suave.Tests/AdapterTests.fs
@@ -6,14 +6,14 @@ open global.Suave
 
 open Fuchu
 
-let test_loggers (min_level : Log.LogLevel) (line_level : Log.LogLevel) (line : LogLine ref) =
+let test_loggers (min_level : Logging.LogLevel) (line_level : Logging.LogLevel) (line : LogLine ref) =
   let stub = { new Logger with
                   member x.Log ll = line := ll
                   member x.Measure m = ()
                   member x.Level = Info
                   member x.Name = "test stub" }
 
-  let subject = SuaveAdapter(stub) :> Suave.Log.Logger
+  let subject = SuaveAdapter(stub) :> Suave.Logging.Logger
 
   subject.Log line_level <| fun () ->
     { message       = "test"
@@ -21,21 +21,21 @@ let test_loggers (min_level : Log.LogLevel) (line_level : Log.LogLevel) (line : 
       level         = line_level
       path          = "test"
       ts_utc_ticks  = 0L
-      trace         = Log.TraceHeader.empty }
+      trace         = Logging.TraceHeader.empty }
 
 [<Tests>]
 let tests =
   testList "with levels" [
     testCase "logs nothing on Debug level" <| fun _ ->
       let line : LogLine ref = ref (LogLine.create'' "a.b.c" "empty")
-      test_loggers Log.Info Log.Debug line
+      test_loggers Logging.LogLevel.Info Logging.LogLevel.Debug line
       Assert.Equal("should have 'empty' message", "empty", (!line).message)
     testCase "logs same on Info level" <| fun _ ->
       let line : LogLine ref = ref (LogLine.create'' "a.b.c" "empty")
-      test_loggers Log.Info Log.Info line
+      test_loggers Logging.LogLevel.Info Logging.LogLevel.Info line
       Assert.Equal("should have 'test' message", "test", (!line).message)
     testCase "logs same on Error level" <| fun _ ->
       let line : LogLine ref = ref (LogLine.create'' "a.b.c" "empty")
-      test_loggers Log.Info Log.Error line
+      test_loggers Logging.LogLevel.Info Logging.LogLevel.Error line
       Assert.Equal("should have 'test' message", "test", (!line).message)
     ]
--- src/Logary.Suave/SuaveAdapter.fs
+++ src/Logary.Suave/SuaveAdapter.fs
@@ -3,29 +3,34 @@
 open Logary
 
 /// Some mapping functions for Suave LogLevels
-module Impl =
+module SuaveLogLevel =
   open System
 
   // deliberatly not opening Suave, to keep types specific
 
   /// Convert a suave log level to a logary log level
-  let to_logary_level : Suave.Log.LogLevel -> Logary.LogLevel = function
-    | Suave.Log.LogLevel.Verbose -> LogLevel.Verbose
-    | Suave.Log.LogLevel.Debug   -> LogLevel.Debug
-    | Suave.Log.LogLevel.Info    -> LogLevel.Info
-    | Suave.Log.LogLevel.Warn    -> LogLevel.Warn
-    | Suave.Log.LogLevel.Error   -> LogLevel.Error
-    | Suave.Log.LogLevel.Fatal   -> LogLevel.Fatal
+  let to_logary : Suave.Logging.LogLevel -> Logary.LogLevel = function
+    | Suave.Logging.LogLevel.Verbose -> LogLevel.Verbose
+    | Suave.Logging.LogLevel.Debug   -> LogLevel.Debug
+    | Suave.Logging.LogLevel.Info    -> LogLevel.Info
+    | Suave.Logging.LogLevel.Warn    -> LogLevel.Warn
+    | Suave.Logging.LogLevel.Error   -> LogLevel.Error
+    | Suave.Logging.LogLevel.Fatal   -> LogLevel.Fatal
+
+module SuaveLogLine =
+  open System
+
+  open NodaTime
 
   /// Convert a Suave LogLine to a Logary LogLine.
-  let to_logary_line (l : Suave.Log.LogLine) =
+  let to_logary (l : Suave.Logging.LogLine) =
     { data          = Map.empty
       message       = l.message
       ``exception`` = l.``exception``
-      level         = l.level |> to_logary_level
+      level         = l.level |> SuaveLogLevel.to_logary
       tags          = []
       path          = l.path
-      timestamp     = NodaTime.Instant.FromDateTimeUtc(DateTime(l.ts_utc_ticks, DateTimeKind.Utc)) }
+      timestamp     =  NodaTime.Instant.FromDateTimeUtc(DateTime(l.ts_utc_ticks, DateTimeKind.Utc)) }
 
 /// An adapter that takes a Logary logger and forwards all Suave logs to it. A simple implementation:
 ///
@@ -33,8 +38,8 @@ module Impl =
 ///   f_line () |> to_logary_line |> Log.log logger
 ///
 type SuaveAdapter(logger : Logger) =
-  interface Suave.Log.Logger with
+  interface Suave.Logging.Logger with
     member x.Log level f_line =
       // here it's important the Level of the logger is well tuned
-      if Impl.to_logary_level level >= logger.Level then
-        f_line () |> Impl.to_logary_line |> Logger.log logger
+      if SuaveLogLevel.to_logary level >= logger.Level then
+        f_line () |> SuaveLogLine.to_logary |> Logger.log logger
