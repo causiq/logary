ff21470c3d00d9336ee537567b33cd1443175de6
Henrik Feldt <henrik@haf.se>
2015-06-26 14:11:50 +0200

    [heka] first cut at Heka target

--- src/targets/Logary.Targets.Heka/Targets_Heka.fs
+++ src/targets/Logary.Targets.Heka/Targets_Heka.fs
@@ -1,5 +1,127 @@
-﻿namespace Logary.Targets.Heka
+module Logary.Targets.Heka
 
-type Class1() = 
-    member this.X = "F#"
+open System
+open System.IO
+open System.Net
+open System.Net.Sockets
+open System.Net.Security
+open FSharp.Actor
+open Logary
+open Logary.Heka
+open Logary.Heka.Client
+open Logary.Heka.Messages
+open Logary.Target
+open Logary.Internals
 
+type Logary.Heka.Messages.Message with
+  static member ofLogLine (line : LogLine) =
+    Message()
+  static member ofMeasure (msr : Measure) =
+    Message()
+
+module internal Impl =
+
+  let logFailure (ri : RuntimeInfo) = function
+    | HeaderTooLarge err ->
+      Logger.warn ri.logger err
+    | MessageTooLarge err ->
+      Logger.warn ri.logger err
+
+  type State =
+    { client : TcpClient
+      stream : Stream }
+
+  let loop (conf : HekaConfig) (ri : RuntimeInfo) (inbox : IActor<_>) =
+    let rec initialise () = async {
+      let ep, useTLS = conf.endpoint
+      let client = new TcpClient(ep)
+      client.NoDelay <- true
+      let stream =
+        if useTLS then
+          let validate = new RemoteCertificateValidationCallback(fun _ -> conf.caValidation)
+          new SslStream(client.GetStream(), false, validate) :> Stream
+        else
+          client.GetStream() :> Stream
+      return! running { client = client; stream = stream }
+      }
+
+    and running state = async {
+      let write msg = async {
+        match msg |> Encoder.encode conf state.stream with
+        | Choice1Of2 run ->
+          do! run
+        | Choice2Of2 err ->
+          logFailure ri err
+        return! running state
+        }
+      let! msg, _ = inbox.Receive()
+      match msg with
+      | Log l ->
+        return! write (Message.ofLogLine l)
+      | Measure msr ->
+        return! write (Message.ofMeasure msr)
+      | Flush ackChan ->
+        ackChan.Reply Ack
+        return! running state
+      | Shutdown ackChan ->
+        return! shutdown state ackChan
+      }
+
+    and shutdown state ackChan =
+      let dispose x = (x :> IDisposable).Dispose()
+      async {
+        Try.safe "riemann target disposing tcp stream, then client" ri.logger <| fun () ->
+          dispose state.stream
+          dispose state.client
+        ackChan.Reply Ack
+      }
+
+    initialise ()
+
+/// Create a new Noop target
+let create conf = TargetUtils.stdNamedTarget (Impl.loop conf)
+
+/// C# Interop: Create a new Noop target
+[<CompiledName "Create">]
+let create' (conf, name) =
+  create conf name
+
+/// Use with LogaryFactory.New( s => s.Target<Heka.Builder>() )
+type Builder(conf, callParent : FactoryApi.ParentCallback<Builder>) =
+  let signConf conf f = // abstract
+    match conf.signingConfig with
+    | None -> { conf with signingConfig = Some (f (MessageSigningConfig.Empty)) }
+    | Some x -> { conf with signingConfig = Some (f x) }
+
+  let buildSignConf conf f = Builder(signConf conf f, callParent) // compose
+  let buildSignConf (f : MessageSigningConfig -> _) = buildSignConf conf f // curry
+
+  /// By default = UInt16.MaxValue + 1 = 65536
+  member x.MaxRecordSize(bytes : uint32) =
+    Builder({ conf with maxMessageSize = bytes }, callParent)
+
+  /// If you want something other than the SHA1-default, call this. Heka supports
+  /// MD5HMAC and SHA1HMAC as of approximately v0.10.
+  member x.HashFunction(hf : HmacHashFunction) =
+    buildSignConf (fun x -> { x with hash = hf })
+
+  /// The Principal Id to authenticate as, and the key to use to sign the
+  /// messages (UTF8.getbytes(key) will be used).
+  member x.SignWith(signerName : string, signingKey : string) =
+    buildSignConf (fun x -> { x with key = signingKey
+                                     name = signerName })
+
+  member x.SignatureMajorVer(majVer : uint32) =
+    buildSignConf (fun x -> { x with version = majVer })
+
+  member x.Endpoint(ep : IPEndPoint, useTLS : UseTLS) =
+    Builder({ conf with endpoint = ep, useTLS }, callParent)
+
+  member x.Done() =
+    ! (callParent x)
+
+  new(callParent : FactoryApi.ParentCallback<_>) =
+    Builder(HekaConfig.Empty, callParent)
+
+  interface Logary.Target.FactoryApi.SpecificTargetConf with
+    member x.Build name = create conf name
--- src/targets/Logary.Targets.Heka/Types.fs
+++ src/targets/Logary.Targets.Heka/Types.fs
@@ -1,8 +1,11 @@
 namespace Logary.Heka
 
 open System
+open System.Net
+open System.Net.Security
 open System.Text
 open System.Security.Cryptography
+open System.Security.Cryptography.X509Certificates
 open Logary.Heka.Messages
 open Logary.Heka.Constants
 
@@ -24,7 +27,7 @@ type MessageSigningConfig =
 
   static member Empty =
     { name    = ""
-      // default to SHA1
+      // default to HMACSHA1, the strongest HMAC algo supported by Heka
       hash    = HmacHashFunction.SHA1
       key     = ""
       version = 0u }
@@ -35,8 +38,12 @@ type MessageSigningConfig =
       key     = key
       version = defaultArg version 0u }
 
+type UseTLS = bool
+
 type HekaConfig =
   { maxMessageSize : uint32
+    endpoint       : IPEndPoint * UseTLS
+    caValidation   : X509Certificate -> X509Chain -> SslPolicyErrors -> bool
     signingConfig  : MessageSigningConfig option }
 
   member x.maxRecordSize =
@@ -45,5 +52,12 @@ type HekaConfig =
     + x.maxMessageSize
 
   static member Empty =
+    let defaultCaValidation : X509Certificate -> X509Chain -> SslPolicyErrors -> bool =
+      fun cer chn err ->
+        ServicePointManager.ServerCertificateValidationCallback.Invoke(null, cer, chn, err)
+
     { maxMessageSize = uint32 (UInt16.MaxValue) + 1u // 64 * 1024
+      // https://hekad.readthedocs.org/en/latest/config/inputs/tcp.html
+      endpoint       = IPEndPoint(IPAddress.IPv6Loopback, 5565), false
+      caValidation   = defaultCaValidation
       signingConfig  = None }
