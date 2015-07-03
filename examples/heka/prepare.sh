232e37fc4e22da92e3c19888aaf02bc6c2756a7a
Henrik Feldt <henrik@haf.se>
2015-07-03 19:26:08 +0200

    [heka] improvements to prepare.sh

--- examples/heka/prepare.sh
+++ examples/heka/prepare.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
-#(cd ../.. && bundle && CONFIGURATION=Debug bundle exec rake build)
+(cd ../.. && mono tools/paket.bootstrapper.exe && \
+             mono tools/paket.exe install && \
+             xbuild /p:Configuration=Debug src/Heka.sln
+)
 mkdir -p ./bin && cp ../../src/targets/Logary.Targets.Heka/bin/Debug/* ./bin
 fsharpc --lib:../../src/targets/Logary.Targets.Heka/bin/Debug -r:Logary.dll -r:Logary.Targets.Heka.dll heka.fs && mv heka.exe bin/
-docker build -t logary/heka-sample .
-
+docker-compose build
