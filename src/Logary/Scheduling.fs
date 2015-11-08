44fed32104732aa2ce23f912d66278ab9c2c1df2
Paavo Huhtala <paavo.huhtala@gmail.com>
2015-11-08 17:50:38 +0200

    Implement Internals.Scheduling with Hopac
    Very early Hopac work. Doesn't remove or modify anything, only adds new code for now.

--- src/Logary/Scheduling.fs
+++ src/Logary/Scheduling.fs
@@ -4,14 +4,105 @@ module Logary.Internals.Scheduling
 
 // creds to Dave Thomas for his F# snippet
 open System.Threading
-  
+
 open FSharp.Actor
 
+open Hopac
+open Hopac.Job.Infixes
+
 open NodaTime
+
+type NamedJob<'a> = NamedJob of name: string * Job<'a>
 
 type ScheduleMsg =
   | Schedule of (obj -> unit) * obj * Duration * Duration * CancellationTokenSource ReplyChannel
   | ScheduleOnce of (obj -> unit) * obj * Duration * CancellationTokenSource ReplyChannel
+
+type Cancellation = {
+  cancelCh: Ch<unit>
+  getCh: Ch<bool>
+}
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Cancellation =
+  let create () =
+    let c = { cancelCh = Ch.Now.create ()
+              getCh    = Ch.Now.create () }
+
+    Job.iterateServer false (fun x ->
+      Alt.choose [Alt.map (fun _ -> false) (Ch.take c.cancelCh)
+                  Alt.map (fun _ -> x)     (Ch.give c.getCh x)])
+    |> Job.start |> ignore
+
+    c
+
+  let get cancellation = job {
+    return! Ch.take cancellation.getCh
+  }
+
+  let cancel cancellation = job {
+    do! Ch.give cancellation.cancelCh ()
+  }
+
+type HopacScheduleMsg =
+  | HopacSchedule of (obj -> unit) * obj * Duration * Duration * Cancellation
+  | HopacScheduleOnce of (obj -> unit) * obj * Duration * Cancellation
+
+module private HopacImpl =
+
+  let ms (d: Duration) =
+    d.ToTimeSpan().TotalMilliseconds |> int
+
+  let schedeluOnce delay msg receiver cts = job {
+    do! timeOutMillis delay
+    let! cancelled = Cancellation.get cts
+
+    if not cancelled then
+      receiver msg
+  }
+
+  let scheduleMany initialDelay msg receiver delayBetween cts = Job.delay <| fun () ->
+    let rec loop time cts = job {
+      do! timeOutMillis time
+      let! cancelled = Cancellation.get cts
+
+      if not cancelled then
+        receiver msg
+        return! loop delayBetween cts
+    }
+
+    loop initialDelay cts
+
+  let loop (ch : Ch<HopacScheduleMsg>) =
+    let rec loop () = job {
+      let! msg = Ch.take ch
+      match msg with
+      | HopacSchedule (receiver, msg : 'a, initialDelay, delayBetween, cts) ->
+        Job.start (scheduleMany (ms initialDelay) msg receiver (ms delayBetween) cts) |> ignore
+        return! loop ()
+      | HopacScheduleOnce (receiver, msg : 'a, delay, cts) ->
+        Job.start (schedeluOnce (ms delay) msg receiver cts) |> ignore
+        return! loop ()
+    }
+
+    loop ()
+
+let hopacCreate () =
+  let ch = Ch.Now.create ()
+  Job.start (HopacImpl.loop ch) |> ignore
+  mb
+
+let hopacSchedule scheduler (receiver : 'a -> unit) (msg : 'a) initialDelay (delayBetween: _ option) =
+  let cts = Cancellation.create ()
+  let message =
+    match delayBetween with
+    | Some x ->
+      HopacSchedule (receiver, msg, initialDelay, x, cts)
+    | None ->
+      HopacScheduleOnce (receiver, msg, initialDelay, cts)
+
+  Ch.send scheduler message |> Job.start |> ignore
+  cts
 
 module private Impl =
 
