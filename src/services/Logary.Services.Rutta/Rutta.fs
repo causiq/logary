7c17f092866f5a2c1957d201564123429e976509
haf <henrik@haf.se>
2016-02-09 19:09:08 +0100

    [rutta] spec, health check done

---
+++ src/services/Logary.Services.Rutta/Rutta.fs
@@ -0,0 +1,244 @@
+﻿module Logary.Services.Rutta
+open System.Reflection
+[<assembly: AssemblyTitle("Logary Rutta – a router/proxy/shipper for Windows and Unix")>]
+()
+
+(* Specification:
+
+This software should be capable of shipping logs to some arbitrary target.
+
+v1: Hard-coded supported target types. Initially we'll just support InfluxDB.
+v2: More configurable target configuration that supports any target.
+
+This service can run in three modes; Shipper, Router and Proxy. Servers can be
+implemented using Hopac's lightweight servers. Communication is implemented
+using ZMQ and a binary serialisation format.
+
+Bindings look may look like this:
+
+  - `Shipper -> Router`
+  - `Shipper -> Proxy`
+  - `Proxy -> Proxy`
+  - `Proxy -> Router`
+
+[ZMQ socket reference](http://api.zeromq.org/3-2:zmq-socket)
+
+The Shipper – from environment to Proxy or Router
+-------------------------------------------------
+
+Enables log shipping from hosts that are not directly connected to the router
+nor to InfluxDB.
+
+Should be spawnable on Unix. Should be service-installable on Windows using
+TopShelf.
+
+### Pushing Shippers
+
+Shippers CONNECT PUSH sockets to the Router's PULL socket.
+See http://lists.zeromq.org/pipermail/zeromq-dev/2012-February/015917.html
+
+``` bash
+./rutta --push-to tcp://headnode:6111
+```
+
+During network splits, the sending
+[PUSH socket blocks](http://api.zeromq.org/3-2:zmq-socket#toc14).
+
+### Publishing Shippers
+
+``` bash
+./rutta --pub-to tcp://headnode:7111
+```
+
+During network splits, the sending XPUSH socket drops messages.
+
+The Proxy – from Shipper to Router
+----------------------------------
+
+Proxies take inputs from Shippers or other Proxies which publish Messages
+using XPUB sockets:
+
+``` bash
+./rutta --pub-to tcp://headnode:7111
+```
+
+The Proxy is run this way, by providing a XSUB socket binding and a XPUB socket
+binding:
+
+``` bash
+./rutta --proxy tcp://10.42.0.1:7111 tcp://192.168.10.10:7112
+```
+
+During network splits, the receiving
+[XSUB socket drops messages](http://api.zeromq.org/3-2:zmq-socket#toc12).
+
+You can then connect to the Proxy with a Router that routes it to the final
+Target (like InfluxDB in this example):
+
+``` bash
+./rutta --router-sub tcp://192.168.10.10:7113 \
+        --router-target influxdb://user:pass@host:8086/write?db=databaseName
+```
+
+During network splits, the sending
+[XPUB socket drops messages](http://api.zeromq.org/3-2:zmq-socket#toc11).
+
+The Router – from Shipper or Proxy to Target
+--------------------------------------------
+
+Implements Fan-In using PULL or SUB of Messages from ZMQ. Forwards internally
+to a Target.
+
+V1 only implements the InfluxDB target.
+
+### Pulling Routers
+
+BINDs a PULL socket on a specified NIC/IP and PORT.
+Configures a single internal Target that pushes the received data.
+
+``` bash
+./rutta --router tcp://192.168.10.10:7113 \
+        --router-target influxdb://user:pass@host:8086/write?db=databaseName
+```
+
+During network splits, the listening
+[PULL socket blocks](http://api.zeromq.org/3-2:zmq-socket#toc15).
+
+Serialisation
+-------------
+
+Should use a fast (network/memory/CPU) serialisation format, preferrably one
+that supports zero copy.
+
+The two competitors are:
+
+ - [FsPickler](https://nessos.github.io/FsPickler/tutorial.html#Picklers-and-Pickler-combinators)
+ - [MessagePack](https://github.com/msgpack/msgpack)
+
+Each ZMQ message contains a Message (see DataModel.fs) in the binary form
+given by the serialiser chosen.
+
+Health Monitoring
+-----------------
+
+Rutta is itself a service that should be
+[health monitored](https://www.consul.io/docs/agent/checks.html), and by default
+it binds to `http://127.0.0.1:8888` for `GET /health` requests. All other
+requests close the socket immediately. You can customise how the health binding
+happens:
+
+``` bash
+./rutta <stuff> --health [::2144:1]:8888
+```
+
+or
+
+``` bash
+./rutta <stuff> --health 192.168.42.144:8888
+```
+
+It will respond with `200 OK`, otherwise `503 Service Unavailable`.
+
+You can configure Consol to health check Rutta:
+
+``` json
+{
+  "check": {
+    "id": "api",
+    "name": "HTTP API on port 8888",
+    "http": "http://127.0.0.1:8888/health",
+    "interval": "5s",
+    "timeout": "500ms"
+  }
+}
+```
+
+*)
+
+open Topshelf
+open Time
+open Logary
+open Argu
+
+type Args =
+  | [<PrintLabels>] Push_To of pushConnectToSocket:string
+  | [<PrintLabels>] Pub_To of pubBindSocket:string
+  | [<PrintLabels>] Router of pullBindSocket:string
+  | [<PrintLabels>] Router_Sub of subConnectSocket:string
+  | [<PrintLabels>] Router_Target of logaryTargetUri:string
+  | [<PrintLabels>] Health of ip:string * port:uint16
+  | [<PrintLabels>] Proxy of xsubConnectToSocket:string * xpubBindSocket:string
+with
+  interface IArgParserTemplate with
+    member s.Usage =
+      match s with
+      | Push_To _ -> "Runs Rutta in Shipper/PUSH mode (send Messages from a node to router)"
+      | Pub_To _ -> "Runs Rutta in Shipper/PUB mode (send Messages from a node to proxy)"
+      | Router _ -> "Runs Rutta in Router mode (PULL fan-in of Messages, forward to Target)."
+      | Router_Sub _ -> "Runs Rutta in Router XSUB mode for PUB sockets to publish to."
+      | Router_Target _ -> "Implied by --router. Specifies where the Router target should forward its data"
+      | Health _ -> "Give Rutta binding information"
+      | Proxy (_,_) -> "Runs Rutta in Proxy mode (XSUB/fan-in of Messages, forward to SUB sockets via XPUB). The first is the XSUB socket (in), the second is the XPUB socket (out)."
+
+module Health =
+  open Suave
+  open Suave.Operators
+  open Suave.Filters
+  open Suave.Successful
+
+  let app =
+    GET >=> path "/health" >=> OK (sprintf "Logary Rutta %s" (YoLo.App.getVersion ()))
+
+  /// Start a Suave web server on the passed ip and port and return the
+  /// cancellation token to use to shut the server down.
+  let startServer ip port =
+    let config =
+      { defaultConfig with bindings = [ HttpBinding.mkSimple HTTP ip port ] }
+
+    let ready, handle = startWebServerAsync config app
+    Async.Start handle
+
+    config.cancellationToken
+
+module Router =
+  ()
+
+module Shipper =
+  ()
+
+module Proxy =
+  ()
+
+[<EntryPoint>]
+let main argv =
+
+  let parser = ArgumentParser.Create<Args>()
+ 
+
+
+  let info : string -> unit = fun s -> Console.WriteLine(sprintf "%s logger/sample-service: %s" (DateTime.UtcNow.ToString("o")) s)
+  let sleep (time : TimeSpan) = System.Threading.Thread.Sleep(time)
+
+  let start hc =
+    info "sample service starting"
+
+    (s 30) |> HostControl.request_more_time hc
+    sleep (s 1)
+
+    Threading.ThreadPool.QueueUserWorkItem(fun cb ->
+        sleep (s 3)
+        info "requesting stop"
+        hc |> HostControl.stop) |> ignore
+
+    info "sample service started"
+    true 
+    
+  let stop hc =
+    info "sample service stopped"
+    true
+  
+  Service.Default
+  |> with_start start
+  |> with_recovery (ServiceRecovery.Default |> restart (min 10))
+  |> with_stop stop
+  |> run
