b771f6d9c4e850da88d63490b6e3b86c0e375824
Henrik Feldt <henrik@haf.se>
2016-02-13 14:52:26 +0100

    [infra] upgrade Suave

--- src/services/Logary.Services.SuaveReporter/SuaveReporter.fs
+++ src/services/Logary.Services.SuaveReporter/SuaveReporter.fs
@@ -1,7 +1,6 @@
 ﻿module Logary.Services.SuaveReporter
 
 open System.Text
-
 open Logary
 open Logary.Utils.Aether
 open Logary.Utils.Aether.Operators
@@ -12,16 +11,7 @@ module Impl =
   open NodaTime
   open NodaTime.Text
   open System
-  open Suave.Http
-
-  type internal Random with
-    /// generate a new random ulong64 value
-    member x.NextUInt64() =
-      let buffer = Array.zeroCreate<byte> sizeof<UInt64>
-      x.NextBytes buffer
-      BitConverter.ToUInt64(buffer, 0)
-
-  let internal r = Random()
+  open Suave
 
   type JsonMsg =
     { message : string
@@ -40,25 +30,26 @@ module Impl =
 open Impl
 open Hopac
 open Suave
-open Suave.Types
 open Suave.Model
-open Suave.Http
-open Suave.Http.Applicatives
-open Suave.Http.RequestErrors
-open Suave.Http.Successful
+open Suave
+open Suave.Filters
+open Suave.RequestErrors
+open Suave.Successful
+open Suave.Operators
+open Suave.Utils
 
 let api (logger : Logger) (verbatimPath : string option) : WebPart =
   let verbatimPath = defaultArg verbatimPath "/i/logary/loglines"
   let getMsg = sprintf "You can post a JSON structure to: %s" verbatimPath
 
   let jsonMsg msg =
-    { message = msg; id = r.NextUInt64() }
+    { message = msg; id = ThreadSafeRandom.nextUInt64 () }
     |> Json.serialize
     |> Json.format
 
-  path verbatimPath >>= choose [
-    GET >>= OK (jsonMsg getMsg)
-    POST >>= Binding.bindReq
+  path verbatimPath >=> choose [
+    GET >=> OK (jsonMsg getMsg)
+    POST >=> Binding.bindReq
               (Lens.get HttpRequest.rawForm_ >> UTF8.toString >> Json.tryParse >> Choice.bind Json.tryDeserialize)
               (fun msg ->
                 Logger.log logger msg |> queue
