302ec96e679435ca6b57466f016a8ce040a8c023
Henrik Feldt <henrik@haf.se>
2016-04-27 16:46:36 +0200

    [csharp] more corrections for C#

--- src/tests/Logary.CSharp.Tests/Message_Specs.cs
+++ src/tests/Logary.CSharp.Tests/Message_Specs.cs
@@ -12,24 +12,27 @@ namespace Logary.Specs
     public class When_changing_properties_on_Message
     {
         static Message subject;
+        static string template;
 
         Establish context = () =>
             {
                 subject = new Message(
                     PointName.NewPointName(new[] { "a", "b", "c" }),
                     PointValue.NewEvent("initial message"),
-                    new Dictionary<string, object>(),
+                    Microsoft.FSharp.Collections.MapModule.Empty<PointName, Field>(),
+                    Microsoft.FSharp.Collections.MapModule.Empty<string, Value>(),
                     LogLevel.Warn,
                     SystemClock.Instance.Now.Ticks * 100L);
+                subject.SetEvent("Hello World").value.TryGetEvent(out template).ShouldBeTrue();
             };
 
         It should_allow_changing_template =
-            () => subject.SetEvent("Hello World").ShouldEqual("Hello World");
+            () => template.ShouldEqual("Hello World");
 
         It should_allow_changing_ts =
             () => subject
                 .SetTimestamp(Instant.FromTicksSinceUnixEpoch(4567))
-                .timestamp
+                .GetTimestamp()
                 .ShouldEqual(Instant.FromTicksSinceUnixEpoch(4567));
     }
 }
