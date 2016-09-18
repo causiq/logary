a17b6406a43daa33c8323404f974252905cef251
Adam Chester <adamchester@gmail.com>
2016-09-19 08:40:49 +1000

    Fix build errors

--- src/Logary/AssemblyInfo.fs
+++ src/Logary/AssemblyInfo.fs
@@ -2,5 +2,6 @@ namespace Logary
 open System.Reflection
 open System.Runtime.CompilerServices
 [<assembly: InternalsVisibleTo("Logary.CSharp")>]
+[<assembly: InternalsVisibleTo("Logary.Adapters.Facade")>]
 [<assembly: Extension>]
 ()
