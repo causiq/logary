2c3cd797d414396f5330654f1157327a71c50c45
Henrik Feldt <henrik@haf.se>
2016-09-21 13:12:54 +0200

    [constants] making constants literals

--- src/Logary/Constants.fs
+++ src/Logary/Constants.fs
@@ -1,15 +1,27 @@
 module Logary.Constants
 
+[<Literal>]
 let SecondsPerTick = 0.0000001
+[<Literal>]
 let MillisPerTick = 0.0001
+[<Literal>]
 let MicrosPerTick = 0.1
+[<Literal>]
 let NanosPerTick = 100L
+[<Literal>]
 let NanosPerMicro = 1000L
+[<Literal>]
 let NanosPerMilli = 1000000L
+[<Literal>]
 let NanosPerSecond = 1000000000L
+[<Literal>]
 let NanosPerMinute = 60000000000L
 
+[<Literal>]
 let TicksPerMinute = 600000000L
+[<Literal>]
 let TicksPerSecond = 10000000L
+[<Literal>]
 let TicksPerMilli = 10000L
+[<Literal>]
 let TicksPerMicro = 10L
