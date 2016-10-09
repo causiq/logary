46d5ba4f5a57f50fc64031cbf33fb9670b0e0a87
Adam Chester <adamchester@gmail.com>
2016-10-09 16:35:34 +1000

    [literate] add literate console basic tests

--- src/tests/Logary.Tests/CoreTargets.fs
+++ src/tests/Logary.Tests/CoreTargets.fs
@@ -4,13 +4,58 @@ open Fuchu
 open Logary
 open Logary.Targets
 open Logary.Tests.Targets
-open Logary.Targets.TextWriter
 open Hopac
 open TestDSL
 open Fac
 
 let textWriterConf =
-  TextWriterConf.create(System.Console.Out, System.Console.Error)
+  TextWriter.TextWriterConf.create(System.Console.Out, System.Console.Error)
+
+module LiterateTesting =
+  open System
+  open LiterateConsole
+  module TestTheme =
+      let textColours =     { foreground=ConsoleColor.White; background=None }
+      let subtextColours =  { foreground=ConsoleColor.Gray; background=None }
+      let punctuationColours = { foreground=ConsoleColor.DarkGray; background=None }
+      let levelVerboseColours = { foreground=ConsoleColor.Gray; background=None }
+      let levelDebugColours = { foreground=ConsoleColor.Gray; background=None }
+      let levelInfoColours = { foreground=ConsoleColor.White; background=None }
+      let levelWarningColours = { foreground=ConsoleColor.Yellow; background=None }
+      let levelErrorColours = { foreground=ConsoleColor.White; background=Some ConsoleColor.Red }
+      let levelFatalColours = { foreground=ConsoleColor.White; background=Some ConsoleColor.Red }
+      let keywordSymbolColours = { foreground=ConsoleColor.Blue; background=None }
+      let numericSymbolColours = { foreground=ConsoleColor.Magenta; background=None }
+      let stringSymbolColours = { foreground=ConsoleColor.Cyan; background=None }
+      let otherSymbolColours = { foreground=ConsoleColor.Green; background=None }
+      let nameSymbolColours = { foreground=ConsoleColor.Gray; background=None }
+      let missingTemplateFieldColours = { foreground=ConsoleColor.Red; background=None }
+      let theme = function
+        | Tokens.Text -> textColours | Tokens.Subtext -> subtextColours | Tokens.Punctuation -> punctuationColours
+        | Tokens.LevelVerbose -> levelVerboseColours | Tokens.LevelDebug -> levelDebugColours
+        | Tokens.LevelInfo -> levelInfoColours | Tokens.LevelWarning -> levelWarningColours
+        | Tokens.LevelError -> levelErrorColours | Tokens.LevelFatal -> levelFatalColours
+        | Tokens.KeywordSymbol -> keywordSymbolColours | Tokens.NumericSymbol -> numericSymbolColours
+        | Tokens.StringSymbol -> stringSymbolColours | Tokens.OtherSymbol -> otherSymbolColours
+        | Tokens.NameSymbol -> nameSymbolColours | Tokens.MissingTemplateField -> missingTemplateFieldColours
+
+  let levelD, levelE, levelF, levelI, levelV, levelW = "D", "E", "F", "I", "V", "W"
+  let singleLetterLogLevelText = function
+    | Debug -> levelD | Error -> levelE | Fatal -> levelF | Info -> levelI | Verbose -> levelV | Warn -> levelW
+
+  let frenchFormatProvider = System.Globalization.CultureInfo("fr-FR") :> IFormatProvider
+
+  /// creates a LiterateConsoleConf for testing purposes, and also returns a function that,
+  /// when invoked, returns the coloured text parts written to the target.
+  let createInspectableWrittenPartsConf () =
+    let writtenParts = ResizeArray<LiterateConsole.ColouredText>()
+    // replace everything except for tokenize
+    { LiterateConsole.empty with
+        formatProvider = frenchFormatProvider
+        getLogLevelText = singleLetterLogLevelText
+        theme = TestTheme.theme
+        colourWriter = (fun sem parts -> writtenParts.AddRange(parts)) },
+    fun () -> writtenParts |> List.ofSeq
 
 [<Tests>]
 let tests =
@@ -18,51 +63,78 @@ let tests =
     Targets.basicTests "text writer" (TextWriter.create textWriterConf)
     Targets.integrationTests "text writer" (TextWriter.create textWriterConf)
 
-    testCase "printing Hello World" <| fun _ ->
-      let stdout = Fac.textWriter ()
-      let target = create (TextWriterConf.create(stdout, stdout)) "writing console target"
-      let instance = target |> Target.init emptyRuntime |> run
-      instance.server (fun _ -> Job.result ()) None
-      |> start
+    Targets.basicTests "literate console" (LiterateConsole.create LiterateConsole.empty)
+    Targets.integrationTests "literate console" (LiterateConsole.create LiterateConsole.empty)
 
-      (because "logging with info level and then finalising the target" <| fun () ->
-        Message.eventInfo "Hello World!" |> Target.logAndWait instance
-        Target.finalise instance
-        stdout.ToString())
-      |> should contain "Hello World!"
-      |> thatsIt
+    testList "literate console" [
+      testCase "printing Hello World" <| fun _ ->
+        let conf, getWrittenParts = LiterateTesting.createInspectableWrittenPartsConf()
+        let target = LiterateConsole.create conf "testLC"
+        let instance = target |> Target.init emptyRuntime |> run
+        instance.server (fun _ -> Job.result ()) None
+        |> start
+        
+        (because "logging with info level and then finalising the target" <| fun () ->
+          Message.eventInfo "Hello World!" |> Message.setTicksEpoch 0L |> Target.logAndWait instance
+          Target.finalise instance
+          getWrittenParts()
+        )
+        |> should equal [ {text = "[";                    colours = LiterateTesting.TestTheme.punctuationColours }
+                          {text = "10:00:00";             colours = LiterateTesting.TestTheme.subtextColours }
+                          {text = " ";                    colours = LiterateTesting.TestTheme.subtextColours }
+                          {text = LiterateTesting.levelI; colours = LiterateTesting.TestTheme.levelInfoColours }
+                          {text = "] ";                   colours = LiterateTesting.TestTheme.punctuationColours }
+                          {text = "Hello World!";         colours = LiterateTesting.TestTheme.textColours } ]
+        |> thatsIt
+    ]
 
-    testCase "initialising TextWriter target" <| fun _ ->
-      let stdout = Fac.textWriter ()
-      let target = create (TextWriterConf.create(stdout, stdout)) "writing console target"
-      let instance = target |> Target.init emptyRuntime |> run
-      instance.server (fun _ -> Job.result ()) None
-      |> start
+    testList "text writer" [
+      testCase "printing Hello World" <| fun _ ->
+        let stdout = Fac.textWriter ()
+        let target = TextWriter.create (TextWriter.TextWriterConf.create(stdout, stdout)) "writing console target"
+        let instance = target |> Target.init emptyRuntime |> run
+        instance.server (fun _ -> Job.result ()) None
+        |> start
 
-      let x = dict ["foo", "bar"]
-      (because "logging with fields then finalising the target" <| fun () ->
-        Message.event Info "Hello World!" |> Message.setFieldFromObject "the Name" x |> Target.logAndWait instance
-        Target.finalise instance
-        stdout.ToString())
-      |> should contain "Hello World!"
-      |> should contain "foo"
-      |> should contain "bar"
-      |> should contain "the Name"
-      |> thatsIt
+        (because "logging with info level and then finalising the target" <| fun () ->
+          Message.eventInfo "Hello World!" |> Target.logAndWait instance
+          Target.finalise instance
+          stdout.ToString())
+        |> should contain "Hello World!"
+        |> thatsIt
 
-    testCase "``error levels should be to error text writer``" <| fun _ ->
-      let out, err = Fac.textWriter (), Fac.textWriter ()
-      let target = create (TextWriterConf.create(out, err)) "error writing"
-      let subject = target |> Target.init emptyRuntime |> run
-      subject.server (fun _ -> Job.result ()) None
-      |> start
+      testCase "initialising TextWriter target" <| fun _ ->
+        let stdout = Fac.textWriter ()
+        let target = TextWriter.create (TextWriter.TextWriterConf.create(stdout, stdout)) "writing console target"
+        let instance = target |> Target.init emptyRuntime |> run
+        instance.server (fun _ -> Job.result ()) None
+        |> start
 
-      (because "logging 'Error line' and 'Fatal line' to the target" <| fun () ->
-        Message.eventError "Error line" |> Target.logAndWait subject
-        Message.eventFatal "Fatal line" |> Target.logAndWait subject
-        Target.finalise subject
-        err.ToString())
-      |> should contain "Error line"
-      |> should contain "Fatal line"
-      |> thatsIt
+        let x = dict ["foo", "bar"]
+        (because "logging with fields then finalising the target" <| fun () ->
+          Message.event Info "Hello World!" |> Message.setFieldFromObject "the Name" x |> Target.logAndWait instance
+          Target.finalise instance
+          stdout.ToString())
+        |> should contain "Hello World!"
+        |> should contain "foo"
+        |> should contain "bar"
+        |> should contain "the Name"
+        |> thatsIt
+
+      testCase "``error levels should be to error text writer``" <| fun _ ->
+        let out, err = Fac.textWriter (), Fac.textWriter ()
+        let target = TextWriter.create (TextWriter.TextWriterConf.create(out, err)) "error writing"
+        let subject = target |> Target.init emptyRuntime |> run
+        subject.server (fun _ -> Job.result ()) None
+        |> start
+
+        (because "logging 'Error line' and 'Fatal line' to the target" <| fun () ->
+          Message.eventError "Error line" |> Target.logAndWait subject
+          Message.eventFatal "Fatal line" |> Target.logAndWait subject
+          Target.finalise subject
+          err.ToString())
+        |> should contain "Error line"
+        |> should contain "Fatal line"
+        |> thatsIt
+      ]
     ]
