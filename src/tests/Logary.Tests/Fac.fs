09e784b302ec2fad3f63bd93fe4afae3ec9ef554
Henrik Feldt <henrik@haf.se>
2016-11-07 12:32:11 +0100

    [tests] extract literal to Fac module

--- src/tests/Logary.Tests/Fac.fs
+++ src/tests/Logary.Tests/Fac.fs
@@ -5,6 +5,7 @@ open System.Text.RegularExpressions
 open System.Text
 open System.Net
 open NodaTime
+open Expecto
 open Logary
 open Logary.Internals
 open Logary.Targets
@@ -12,17 +13,19 @@ open Logary.Targets.TextWriter
 open Logary.Configuration
 open Logary.Target
 open TestDSL
-open Expecto
 open Hopac
 open Hopac.Infixes
 
 let emptyTarget = Noop.create {isYes = true} "empty target"
 let emptyRule = Rule.createForTarget "empty target"
-let emptyRuntime =
-  { serviceName = "tests"
-    host        = Dns.GetHostName ()
-    clock       = SystemClock.Instance
-    logger      = NullLogger() }
+let emptyRuntime = RuntimeInfo.create "tests"
+
+let literal () =
+  LiterateConsole.create LiterateConsole.empty "literate"
+  |> Target.init emptyRuntime
+  >>= (fun ti -> Job.start (ti.server (fun _ -> Job.result ()) None) >>-. ti)
+  >>- (Seq.singleton >> InternalLogger.create LogLevel.Debug)
+  |> run
 
 let textWriter () =
   let sb = new StringBuilder()
--- src/tests/Logary.Tests/Program.fs
+++ src/tests/Logary.Tests/Program.fs
@@ -1,4 +1,4 @@
-﻿module Program
+﻿module Logary.Tests.Program
 
 open System.Globalization
 open System.Threading
@@ -7,20 +7,11 @@ open Hopac
 open Hopac.Infixes
 open Logary
 open Logary.Adapters.Facade
-open Logary.Internals
-open Logary.Tests.Fac
-open Logary.Targets
 
 [<EntryPoint>]
 let main args =
   let enUS = CultureInfo "en-US"
-  let literal =
-    LiterateConsole.create LiterateConsole.empty "literate"
-    |> Target.init { emptyRuntime with serviceName = "tests" }
-    >>= (fun ti -> Job.start (ti.server (fun _ -> Job.result ()) None) >>-. ti)
-    >>- (Seq.singleton >> InternalLogger.create Debug)
-    |> run
-  let adapter = LoggerAdapter.createGeneric<Expecto.Logging.Logger> literal
+  let adapter = LoggerAdapter.createGeneric<Expecto.Logging.Logger> (Fac.literal ())
   Expecto.Logging.Global.initialise { Expecto.Logging.Global.DefaultConfig with getLogger = fun _ -> adapter }
   Thread.CurrentThread.CurrentCulture   <- enUS
   Thread.CurrentThread.CurrentUICulture <- enUS
