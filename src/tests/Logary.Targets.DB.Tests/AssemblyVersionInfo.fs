f16abc1ddf9828812784035fc9693eb7aaf1cb07
Henrik Feldt <henrik@haf.se>
2016-11-08 16:25:45 +0100

    [infra] adding all AssemblyVersionInfos

---
+++ src/tests/Logary.Targets.DB.Tests/AssemblyVersionInfo.fs
@@ -0,0 +1,13 @@
+namespace Logary.Targets.DB.TestsAsmVer
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+[<assembly: AssemblyDescription("Logary is a high performance, multi-target logging, metric and health-check library for mono and .Net.")>]
+[<assembly: AssemblyConfiguration("Release")>]
+[<assembly: AssemblyCompany("Logibit AB")>]
+[<assembly: AssemblyCopyright("(c) 2016 by Henrik Feldt")>]
+[<assembly: AssemblyVersion("4.0.200.0")>]
+[<assembly: AssemblyFileVersion("4.0.200.0")>]
+[<assembly: AssemblyInformationalVersion("4.0.200.33bfa3")>]
+[<assembly: AssemblyTitle("Logary.Targets.DB.Tests")>]
+()
