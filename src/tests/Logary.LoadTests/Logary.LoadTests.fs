e5160db35dbe86127ae4e05f648ff9e546a77776
Henrik Feldt <henrik@haf.se>
2017-01-09 15:07:40 +0100

    [tests] adding load tests

---
+++ src/tests/Logary.LoadTests/Logary.LoadTests.fs
@@ -0,0 +1,13 @@
+module Logary.LoadTests
+
+open Logary
+open Expecto
+
+[<Tests>]
+let tests =
+  testList "" [
+  ]
+
+[<EntryPoint>]
+let main argv =
+  Tests.runTestsInAssembly defaultConfig argv
