2a557eff40c72f7962dc553d3498a6f2ac09fa07
Henrik <henrik@haf.se>
2018-04-04 13:09:53 +0200

    First cut at UDP listener

---
+++ src/ingestion/Logary.Ingestion.Http/Http.fs
@@ -0,0 +1,49 @@
+﻿module Logary.Ingestion.Http
+
+open System.Text
+open Hopac
+open Logary
+open Logary.Internals.Chiron
+open Suave
+open Suave.Filters
+open Suave.Writers
+open Suave.RequestErrors
+open Suave.Successful
+open Suave.Operators
+
+type ReporterConfig =
+  { rootPath: string
+    next: Json -> Job<unit> }
+  static member create rootPath next =
+    { rootPath = rootPath; next = next }
+
+let private printHelp (config: ReporterConfig): WebPart =
+  let message = sprintf "You can post a JSON Objects to: %s" config.rootPath
+  OK message >=> setMimeType "text/plain; charset=utf-8"
+
+let private thx = ACCEPTED "true"
+let private fail = Json.encode >> BAD_REQUEST
+
+let private logMessage (config: ReporterConfig): WebPart =
+  fun ctx ->
+    async {
+      match Json.parse (Encoding.UTF8.GetString ctx.request.rawForm) with
+      | JsonResult.JPass json ->
+        do! Job.toAsync (config.next json)
+        return! thx ctx
+      | JsonResult.JFail err ->
+        return! fail err ctx
+    }
+
+let defaultConfig =
+  let logger = Log.create "SuaveReporter"
+  ReporterConfig.create "/i/logary" logger
+
+let api (config: ReporterConfig): WebPart =
+  choose [
+    GET >=> path config.rootPath >=> printHelp config
+    POST >=>  path config.rootPath >=> logMessage config >=> setMimeType "application/json; charset=utf-8"
+  ]
+
+let create (config: ReporterConfig) =
+  startWebServer Suave.Web.defaultConfig (api config)
---
+++ src/ingestion/Logary.Ingestion.UDP/UDP.fs
@@ -0,0 +1,48 @@
+﻿module Logary.Ingestion.UDP
+
+open Logary
+open Logary.Message
+open Hopac
+open System
+open System.Net
+open System.Net.Sockets
+
+type UDPConfig =
+  { endpoint: IPEndPoint
+    next: byte[] -> Job<unit> }
+
+let private ilogger = Log.create "LogClient"
+
+let private handleDisposed (xJ: Job<unit>) =
+  Job.tryWith xJ (function
+    // https://stackoverflow.com/a/18314614/63621
+    | :? ObjectDisposedException -> Job.result ()
+    | e -> Job.raises e)
+
+type UdpClient with
+  member internal x.getMessage () =
+    Job.fromTask (fun () -> x.ReceiveAsync())
+    |> Job.map (fun d -> d.Buffer)
+
+/// Creates a new LogClient with an address, port and a sink (next.)
+let create (config: UDPConfig) =
+  let rec receiveLoop (inSocket: UdpClient) =
+    job {
+      let! msg = inSocket.getMessage()
+      do! config.next msg
+      return! receiveLoop inSocket
+    }
+
+  job {
+    do ilogger.info (eventX "Starting UDP log listener at {endpoint}" >> setField "endpoint" config.endpoint)
+    use inSocket = new UdpClient(config.endpoint)
+    try
+      do! handleDisposed (receiveLoop inSocket)
+    finally
+      do ilogger.info (eventX "Stopping UDP log listener at {endpoint}" >> setField "endpoint" config.endpoint)
+      try inSocket.Close()
+      with _ -> ()
+  }
+
+  // https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.udpclient.receiveasync?view=netframework-4.7.1#System_Net_Sockets_UdpClient_ReceiveAsync
+  // exceptions not handled: https://msdn.microsoft.com/en-us/library/system.net.sockets.socketexception.errorcode(v=vs.110).aspx
