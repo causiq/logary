5c788b4b5f90873f5bf90a7c7edfba05f90cd19e
Adam Chester <adamchester@gmail.com>
2018-04-08 10:29:31 +1000

    Remove `isNull`

--- src/Logary.Facade/Facade.fs
+++ src/Logary.Facade/Facade.fs
@@ -299,14 +299,6 @@ type LoggingConfig =
     /// adapter to replace this semaphore with a global semaphore.
     consoleSemaphore: obj }
 
-[<AutoOpen>]
-module internal Helpers = 
-  /// replaces the built-in `isNull` function so we can still use it when < FSharp.Core 4.0
-  let inline isNull o =
-    match o with
-    | null -> true
-    | _ -> false
-
 module Literate =
   /// The output tokens, which can be potentially coloured.
   type LiterateToken =
