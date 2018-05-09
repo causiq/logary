7019522043a8a2896ee20a12a69b49924e9ecf12
Henrik Feldt <henrik@haf.se>
2018-05-09 18:00:34 +0200

    Create a new BadBoy target, parametise benchmarks on logary config

--- src/Logary.PerfTests/Program.fs
+++ src/Logary.PerfTests/Program.fs
@@ -1,17 +1,14 @@
-module Logary.PerfTests.Program
+namespace Logary.PerfTests
 
+open System
 open System.Threading
 open Expecto
 open Expecto.Flip
-
 open Logary
 open Logary.Configuration
 open Logary.Message
-open System
-
-let run = Hopac.Hopac.run
-
 open BenchmarkDotNet
+open BenchmarkDotNet.Code
 open BenchmarkDotNet.Jobs
 open BenchmarkDotNet.Attributes
 open BenchmarkDotNet.Attributes.Jobs
@@ -19,13 +16,24 @@ open BenchmarkDotNet.Attributes.Exporters
 open BenchmarkDotNet.Attributes.Columns
 open BenchmarkDotNet.Attributes.Jobs
 
-let baseJob =
-  Job.Default
-    .WithInvocationCount(1024)
-    .WithWarmupCount(16)
-    .WithLaunchCount(1)
-    .WithGcServer(true)
-    .WithGcConcurrent(true)
+[<AutoOpen>]
+module Values =
+  let run = Hopac.Hopac.run
+  let targets =
+    Map [
+      "noop", Targets.Noop.create Targets.Noop.empty "sink"
+      "badboy_single_nodelay", Targets.BadBoy.create Targets.BadBoy.empty "sink"
+      "badboy_single_delay", Targets.BadBoy.create Targets.BadBoy.empty "sink"
+      "badboy_batch_nodelay", Targets.BadBoy.create Targets.BadBoy.empty "sink"
+      "badboy_batch_delay", Targets.BadBoy.create Targets.BadBoy.empty "sink"
+    ]
+  let baseJob =
+    Job.Default
+      .WithInvocationCount(20480)
+      .WithWarmupCount(16)
+      .WithLaunchCount(1)
+      .WithGcServer(true)
+      .WithGcConcurrent(true)
 
 type ClrJobAttribute(isBaseline) =
   inherit JobConfigBaseAttribute(Job(Job.Clr, baseJob))
@@ -35,43 +43,68 @@ type CoreJobAttribute(isBaseline) =
   inherit JobConfigBaseAttribute(Job(Job.Core, baseJob))
   new() = CoreJobAttribute(false)
 
+[<Struct>]
+type LogaryValue =
+  val logger: Logger
+  val target: string
+  new (target: string) =
+    let logary =
+      Config.create "Logary.ConsoleApp" "localhost"
+      |> Config.targets [ targets |> Map.find target ]
+      |> Config.ilogger (ILogger.Console Warn)
+      |> Config.processing (Events.events |> Events.sink [ "sink" ])
+      |> Config.build
+      |> run
+    { logger = logary.getLogger (PointName [| "PerfTestLogger" |])
+      target = target }
+
+type LogaryParam(value: LogaryValue) =
+  interface IParam with
+    member x.Value = box value
+    member x.DisplayText = sprintf "Logary with target=%s" value.target
+    member x.ToSourceCode() = sprintf "new Logary.PerTests.LogaryValue(%s)" value.target
+
+[<MemoryDiagnoser>]
 [<ClrJob; CoreJob(isBaseline=true)>]
 [<RPlotExporter; RankColumn>]
 type LogEndToEndNoOutput() =
-  let logary =
-    Config.create "Logary.ConsoleApp" "localhost"
-    |> Config.targets [ Targets.Noop.create Targets.Noop.empty "noop" ]
-    |> Config.ilogger (ILogger.Console Warn)
-    |> Config.processing (Events.events |> Events.sink [ "noop" ])
-    |> Config.build
-    |> run
 
-  let logger = logary.getLogger (PointName [| "EndToEndNoOutput" |])
+  [<ParamsSource("Configs"); DefaultValue>]
+  val mutable logary: LogaryValue
+
+  let toParam (x: IParam) = x
+
+  member x.Configs() =
+    [ "noop"; "badboy_single_nodelay" ]
+    |> Seq.map (LogaryValue >> LogaryParam >> toParam)
 
   [<Benchmark>]
   member x.LogHelloWorld_WithAck() =
-    run (logger.debugWithAck (eventX "Hello world"))
+    run (x.logary.logger.debugWithAck (eventX "Hello world"))
 
   [<Benchmark>]
   member x.LogHelloWorld_WithBP() =
-    run (logger.debugWithBP (eventX "Hello world"))
+    run (x.logary.logger.debugWithBP (eventX "Hello world"))
 
   [<Benchmark>]
   member x.LogHelloWorld_Simple() =
-    logger.logSimple (eventX "Hello world" Debug)
+    x.logary.logger.logSimple (eventX "Hello world" Debug)
 
-[<Tests>]
-let benchmarks =
-  testList "benchmarks" [
-    test "log end to end with no output" {
-      benchmark<LogEndToEndNoOutput> benchmarkConfig (fun summary -> null)
-        |> ignore
-    }
-  ]
+module Tests =
+  [<Tests>]
+  let benchmarks =
+    testList "benchmarks" [
+      test "noop sink - baseline" {
+        let summary =
+          benchmark<LogEndToEndNoOutput> benchmarkConfig (id >> box)
+        ()
+      }
+    ]
 
-[<EntryPoint>]
-let main argv =
-  Environment.SetEnvironmentVariable("System.GC.Server", "true")
-  Environment.SetEnvironmentVariable("gcServer", "1")
-  use cts = new CancellationTokenSource()
-  runTestsInAssemblyWithCancel cts.Token defaultConfig argv
+module Program =
+  [<EntryPoint>]
+  let main argv =
+    Environment.SetEnvironmentVariable("System.GC.Server", "true")
+    Environment.SetEnvironmentVariable("gcServer", "1")
+    use cts = new CancellationTokenSource()
+    runTestsInAssemblyWithCancel cts.Token defaultConfig argv
---
+++ src/Logary/Targets/BadBoy.fs
@@ -0,0 +1,114 @@
+/// A module to test how your app reacts to logging to a very slow target.
+module Logary.Targets.BadBoy
+
+open Hopac
+open Hopac.Infixes
+open Logary
+open Logary.Internals
+open Logary.Configuration.Target
+open NodaTime
+
+type BadBoyConf =
+  { /// Delay every message / every message batch (if batching) this much
+    delay: Duration
+    batch: bool }
+
+let empty =
+  { delay = Duration.FromMilliseconds 400L
+    batch = true }
+
+module internal Impl =
+  let loop (conf: BadBoyConf) (api: TargetAPI): Job<_> =
+    let rec singleLoopDelay () =
+      let take =
+        RingBuffer.take api.requests ^=> function
+          | Log (message, ack) ->
+            timeOut (conf.delay.ToTimeSpan())
+            >>=. ack *<= ()
+            >>= singleLoopDelay
+          | Flush (ack, nack) ->
+            let simulateWrite = timeOut (conf.delay.ToTimeSpan()) ^=>. IVar.fill ack ()
+            (simulateWrite <|> nack) >>= singleLoopDelay
+
+      let shutdown =
+        api.shutdownCh ^=> fun ack -> IVar.fill ack ()
+
+      take <|> shutdown
+
+    let rec singleLoop () =
+      let take =
+        RingBuffer.take api.requests ^=> function
+          | Log (message, ack) -> IVar.fill ack () >>= singleLoop
+          | Flush (ack, _) -> IVar.fill ack () >>= singleLoop
+
+      let shutdown =
+        api.shutdownCh ^=> fun ack -> IVar.fill ack ()
+
+      take <|> shutdown
+
+    let rec batchLoopDelay () =
+      let take =
+        RingBuffer.takeBatch 256us api.requests ^=> fun messages ->
+          let entries, acks, flushes =
+            // Make a single pass over the array, accumulating the entries, acks and flushes.
+            messages |> Array.fold (fun (entries, acks, flushes) -> function
+              | Log (message, ack) ->
+                message :: entries,
+                ack *<= () :: acks,
+                flushes
+              | Flush (ackCh, nack) ->
+                entries,
+                acks,
+                ackCh *<= () :: flushes)
+              ([], [], [])
+
+          timeOut (conf.delay.ToTimeSpan()) ^=> fun () ->
+          Job.conIgnore acks >>=. Job.conIgnore flushes >>= batchLoopDelay
+
+      let shutdown =
+        api.shutdownCh ^=> fun ack -> IVar.fill ack ()
+
+      take <|> shutdown
+
+    let batchLoop () =
+      let take =
+        RingBuffer.takeBatch 256us api.requests ^=> fun messages ->
+          let entries, acks, flushes =
+            // Make a single pass over the array, accumulating the entries, acks and flushes.
+            messages |> Array.fold (fun (entries, acks, flushes) -> function
+              | Log (message, ack) ->
+                message :: entries,
+                ack *<= () :: acks,
+                flushes
+              | Flush (ackCh, nack) ->
+                entries,
+                acks,
+                ackCh *<= () :: flushes)
+              ([], [], [])
+          Job.conIgnore acks >>=. Job.conIgnore flushes >>= batchLoopDelay
+
+      let shutdown =
+        api.shutdownCh ^=> fun ack -> IVar.fill ack ()
+
+      take <|> shutdown
+
+    if conf.batch then
+      if conf.delay <> Duration.Zero then
+        upcast singleLoopDelay ()
+      else
+        upcast singleLoop ()
+    else
+      if conf.delay <> Duration.Zero then
+        upcast batchLoopDelay ()
+      else
+        upcast batchLoop ()
+
+[<CompiledName "Create">]
+let create conf name = TargetConf.createSimple (Impl.loop conf) name
+
+type Builder(conf, callParent: ParentCallback<Builder>) =
+  let update (conf': BadBoyConf): Builder = Builder(conf', callParent)
+  member x.NoBatch() = update { conf with batch = false }
+  member x.Delay(delay: Duration) = update { conf with delay = delay }
+  new(callParent: ParentCallback<_>) = Builder(empty, callParent)
+  interface SpecificTargetConf with member x.Build name = create conf name
