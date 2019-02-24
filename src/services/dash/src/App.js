c759ab92d404869eecf7f76564431d4c74f209c8
Henrik Feldt <henrik@haf.se>
2019-02-24 16:11:50 +0100

    Handle batched events from SSE, add Dash license, add Dash reconnection

--- src/services/dash/src/App.js
+++ src/services/dash/src/App.js
@@ -4,8 +4,8 @@ import classNames from 'classnames'
 import CircularBuffer from './lib/circularBuffer'
 import subscribe from './lib/subscribe'
 import sse from './lib/sse'
-import { Scheduler, BehaviorSubject, interval } from 'rxjs'
-import { scan, map, auditTime, combineLatest, startWith } from 'rxjs/operators'
+import { Scheduler, BehaviorSubject, interval, of, from } from 'rxjs'
+import { scan, map, auditTime, combineLatest, startWith, retryWhen, delay, concatMap } from 'rxjs/operators'
 import "./App.css"
 
 function template(value, fields = {}, context = {}) {
@@ -15,25 +15,27 @@ function template(value, fields = {}, context = {}) {
   })
 }
 
-const interpret = m => ({
-  ...m,
-  message: template(m.value, m.fields, m.context)
-})
+const interpret = input => {
+  return Array.isArray(input)
+    ? from(input.map(x => ({ ...x, message: template(x.value, x.fields, x.context) })))
+    : of({ ...input, message: template(input.value, input.fields, input.context) })
+}
 
 const allLogs$ = sse("http://localhost:8080/logs").pipe(
+  retryWhen(e => e.pipe(delay(1000))),
   map(JSON.parse),
-  map(interpret),
+  concatMap(interpret),
   scan((acc, x, i) => acc.push(x), new CircularBuffer(2048)),
   auditTime(0, Scheduler.animationFrameScheduler), // https://rxjs-dev.firebaseapp.com/api/operators/auditTime
   map(xs => xs.snapshot())
 )
 
 const MessageTableInner = (props) => {
-  if (props.message$.isLoading) {
+  if (props.messages.isLoading) {
     return "Loading..."
   }
 
-  const rows = props.message$.value.map((message, i) =>
+  const rows = props.messages.value.map((message, i) =>
     <tr className={classNames(`level-${message.level}`)} key={`message-${i}`}>
       <td className='level'>{message.level}</td>
       <td className='message' title={message.value}>{message.message}</td>
@@ -68,12 +70,13 @@ const filter$ = new BehaviorSubject('')
 const Filter = subscribe({ filter$ }, { setFilter: x => filter$.next(x) })(FilterInput)
 const age = interval(1000).pipe(startWith(0))
 
-const message$ = allLogs$.pipe(
+const messages = allLogs$.pipe(
   combineLatest(filter$),
   map(([ xs, filter ]) =>
-    xs.filter(x => x.level === filter || x.message.indexOf(filter) !== -1 || x.name.indexOf(filter) !== -1))
+    xs.filter(x => x.level === filter || x.message.indexOf(filter) !== -1 || x.name.indexOf(filter) !== -1)
+  )
 )
-const MessageTable = subscribe({ message$, age })(MessageTableInner)
+const MessageTable = subscribe({ messages, age })(MessageTableInner)
 
 const App = () => {
   return <React.Fragment>
