9355e83011d460c97609cca918c9a3b8763c4af8
Henrik Feldt <henrik@haf.se>
2019-02-24 19:07:13 +0100

    Convert styling to emotion

--- src/services/dash/src/App.js
+++ src/services/dash/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import moment from 'moment'
-import classNames from 'classnames'
 import CircularBuffer from './lib/circularBuffer'
 import subscribe from './lib/subscribe'
 import sse from './lib/sse'
 import { Scheduler, BehaviorSubject, interval, of, from } from 'rxjs'
 import { scan, map, auditTime, combineLatest, startWith, retryWhen, delay, concatMap } from 'rxjs/operators'
-import "./App.css"
+
+/** @jsx jsx */
+import { Global, jsx } from '@emotion/core'
+import styled, { css } from '@emotion/primitives'
+import { ThemeProvider } from 'emotion-theming'
+
+////////////////////////// DATA //////////////////////
+
+// Nice: https://usehooks.com/
 
 function template(value, fields = {}, context = {}) {
   return value.replace(/\{(\w+)\}/g, (match, key, y, z) => {
@@ -30,44 +37,7 @@ const allLogs$ = sse("http://localhost:8080/logs").pipe(
   map(xs => xs.snapshot())
 )
 
-const MessageTableInner = (props) => {
-  if (props.messages.isLoading) {
-    return "Loading..."
-  }
-
-  const rows = props.messages.value.map((message, i) =>
-    <tr className={classNames(`level-${message.level}`)} key={`message-${i}`}>
-      <td className='level'>{message.level}</td>
-      <td className='message' title={message.value}>{message.message}</td>
-      <td className='name'>{message.name}</td>
-      <td className='timestamp' title={message.timestamp}>{moment(message.timestamp).fromNow()}</td>
-    </tr>)
-
-  return <table data-age={props.age.value}>
-    <thead>
-      <tr>
-        <th className='level'>Level</th>
-        <th className='message'>Message</th>
-        <th className='name'>Name</th>
-        <th className='timestamp'>Timestamp</th>
-      </tr>
-    </thead>
-    <tbody>
-      {rows}
-    </tbody>
-  </table>
-}
-
-const FilterInput = ({ filter, setFilter }) =>
-  <input
-    id="filter"
-    value={filter}
-    onChange={evt => setFilter(evt.currentTarget.value)}
-    placeholder="Filter logs..."
-    />
-
 const filter$ = new BehaviorSubject('')
-const Filter = subscribe({ filter$ }, { setFilter: x => filter$.next(x) })(FilterInput)
 const age = interval(1000).pipe(startWith(0))
 
 const messages = allLogs$.pipe(
@@ -76,16 +46,184 @@ const messages = allLogs$.pipe(
     xs.filter(x => x.level === filter || x.message.indexOf(filter) !== -1 || x.name.indexOf(filter) !== -1)
   )
 )
+
+////////////////////////// VIEWS //////////////////////
+
+const theme = {
+  bg: '#302F34',  // dark black blue
+  bg2: '#242424', // dark grey
+  text: '#D5E9FA', // white blue
+  highlight: '#306DAC', // matte dark blue
+  highlightText: '#D9EDFF', // whiter blue
+  info: '#1B5A75',
+  infoFocus: '#2E9AC9', // carrebean ocean blue
+  warning: '#DB8C1F', // organge
+  warningFocus: '#DB8C1F', // brighter orange
+  error: '#CC5A54', // red
+  errorFocus: '#DE625B' // brighter red
+}
+
+const FilterInput = ({ filter, setFilter }) =>
+  <input
+    id="filter"
+    css={css({
+      padding: '0.6em 0',
+      flex: 1
+    })}
+    value={filter}
+    onChange={evt => setFilter(evt.currentTarget.value)}
+    placeholder="Filter logs..."
+    />
+
+const Filter = subscribe({ filter$ }, { setFilter: x => filter$.next(x) })(FilterInput)
+
+/**
+ * Wrappers around cells and other wrappers: flex, row, wrap
+ */
+const wrapper = css({
+  display: 'flex',
+  flexDirection: 'row',
+  flexGrow: 0,
+  flexWrap: 'wrap'
+})
+
+/**
+ * Text based cells: truncate and show ellipsis when there is not enough space
+ */
+const text = css({
+  overflow: 'hidden',
+  whiteSpace: 'nowrap',
+  paddingRight: '20px',
+})
+
+const longLine = css({
+  width: '180px',
+  flexGrow: 20
+})
+
+const shortLine = css({
+  width: '100px',
+  flexGrow: 10
+})
+
+const xshortLine = css({
+  width: '80px',
+  flexGrow: 5,
+})
+
+const tinyLine = css({
+  width: '50px',
+  flexGrow: 1
+})
+
+const header = css({
+  textAlign: 'left',
+  textTransform: 'uppercase',
+  backgroundColor: theme.bg2,
+  fontWeight: 'bold'
+})
+
+const Cell = styled.Text({
+  ...text,
+  padding: '1em',
+})
+
+/**
+ * Main container: initialize the flex, direction is row
+ */
+const Row = styled.View({
+  display: 'flex',
+  flexDirection: 'row',
+  flexGrow: 0,
+  width: '100%',
+  '&:hover': {
+    border: `1px solid ${theme.highlight}`
+  }
+})
+
+const Table = styled.View({})
+
+// https://codepen.io/Orangetronic/pen/pJgpXw
+// https://scotch.io/tutorials/a-visual-guide-to-css3-flexbox-properties#toc-flex-grow
+// https://hashnode.com/post/really-responsive-tables-using-css3-flexbox-cijzbxd8n00pwvm53sl4l42cx
+// https://codepen.io/vasansr/pen/VerOPy?editors=1100
+const MessageTableInner = ({
+  messages,
+  age,
+  ...rest
+}) => {
+  if (messages.isLoading) {
+    return "Loading..."
+  }
+
+  const rows = messages.value.map((message, i) => {
+    const colourised = css({
+      backgroundColor: theme[message.level] || 'inherit'
+    })
+    return <Row key={`message-${i}`}>
+      <Cell css={[ tinyLine, colourised ]} data-name='level'>{message.level}</Cell>
+      <Cell css={[ longLine ]} data-name='message' title={message.value}>{message.message}</Cell>
+      <Cell css={[ shortLine ]} data-name='name'>{message.name}</Cell>
+      <Cell css={[ xshortLine ]} data-name='timestamp' title={message.timestamp}>{moment(message.timestamp).fromNow()}</Cell>
+    </Row>})
+
+  return <Table data-age={age.value} css={css({
+    '> *:nth-child(2n+1)': {
+      backgroundColor: theme.bg2
+    },
+  })} {...rest}>
+    <Row css={[ wrapper, header ]}>
+      <Cell css={[ tinyLine ]} className='level'>Level</Cell>
+      <Cell css={[ longLine ]} className='message'>Message</Cell>
+      <Cell css={[ shortLine ]} className='name'>Name</Cell>
+      <Cell css={[ xshortLine ]} className='timestamp'>Timestamp</Cell>
+    </Row>
+    {rows}
+  </Table>
+}
+
 const MessageTable = subscribe({ messages, age })(MessageTableInner)
 
+const globalStyles = css({
+  body: {
+    backgroundColor: theme.bg,
+    minHeight: '100vh',
+    fontSize: '10px',
+    color: theme.text,
+    minWidth: '350px'
+  },
+  '#root': {
+    fontSize: '130%'
+  },
+  '::selection': {
+    backgroundColor: theme.highlight,
+    color: theme.highlightText
+  }
+})
+
+const H1 = styled.Text`
+  margin: 0 14px;
+  padding: 0;
+  font-size: 200%;
+  font-weight: bold
+`
+
+const Header = styled.View({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  padding: '1vmin'
+})
+
 const App = () => {
-  return <React.Fragment>
-    <div id="header">
-      <h1>Logary Dash</h1>
+  return <ThemeProvider theme={theme}>
+    <Global styles={globalStyles} />
+    <Header id="header">
+      <H1>Logary Dash</H1>
       <Filter />
-    </div>
-    <MessageTable />
-  </React.Fragment>
+    </Header>
+    <MessageTable css={css({ width: '100%' })} />
+  </ThemeProvider>
 }
 
 export default App;
