9f4c1b45af459b5999ea95dd6c8348c3e1976593
Henrik Feldt <henrik@haf.se>
2019-04-03 15:04:00 +0200

    Permissions for writing to the logs dataset in BQ

--- tools/setup-ci.sh
+++ tools/setup-ci.sh
@@ -1,5 +1,9 @@
 PROJECT_ID="crucial-baton-203418"
+EMAIL="logary-ci@${PROJECT_ID}.iam.gserviceaccount.com"
 gcloud iam service-accounts create "logary-ci" --display-name "logary-ci"
-gcloud iam service-accounts keys create "logary-ci.json" --iam-account "logary-ci@${PROJECT_ID}.iam.gserviceaccount.com"
+gcloud iam service-accounts keys create "logary-ci.json" --iam-account "$EMAIL"
 gcloud pubsub topics create logs
 gcloud iam roles create logary.ci --project "${PROJECT_ID}" --file logary-ci-role.yaml
+bq --format=json show logs | jq ". * {access: [.access[], {\"role\":\"WRITER\",\"userByEmail\":\"$EMAIL\"}] }" >logary-ci-bq.json
+bq update --source=logary-ci-bq.json logs
+rm logary-ci-bq.json
