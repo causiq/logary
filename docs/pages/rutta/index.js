913715eff5a9885ebfa5a82118d06a71adff93b2
Henrik Feldt <henrik@haf.se>
2019-04-11 15:13:15 +0200

    Improve Rutta docs

--- docs/pages/rutta/index.js
+++ docs/pages/rutta/index.js
@@ -21,8 +21,8 @@ export default function Rutta() {
         <p>
           The first step is to install the Rutta Helm chart into your Kubernetes cluster.
         </p>
-        <p>Rutta is a high-performance log router/shipper written in F# but configured from the command line. It's "cloud native" in that it runs as a docker container on top of .Net Core.</p>
-        <p>Rutta is GPLv3 licensed (or alternatively commercially licensed). It's a packaging of Logary as a service, that you can run, either as a sidecar container or as a log router in the middle of things.</p>
+        <p>Rutta is a high-performance log router/shipper written in F# but configured from the command line. It's "cloud native" in that it runs as a docker container on top of .Net Core</p>
+        <p>Rutta is GPLv3 licensed (or alternatively commercially licensed). It's a packaging of Logary as a service, that you can run, either as a sidecar container or as a log router deployment.</p>
         <p>Rutta is completely stateless, so you can run any number of replicas. It runs as a Kubernetes deployment with three replicas by default.</p>
         <p>A common configuration for Rutta is to configure a Stackdriver target, a HTTP ingestion listener as well as a UDP ingestion listener. Your apps send UDP log messages to the UDP endpoint and your frontends (native apps and web sites) send HTTP messages to the HTTP endpoint. Rutta when batch-ships these log messages into Stackdriver.</p>
         <p>By default this chart exposes a HTTP listener/endpoint and prints to console; in order for it to log to Stackdriver, AliYun or AppInsights, you have to configure those explicitly in the values file.</p>
@@ -33,12 +33,32 @@ export default function Rutta() {
         <p>
           The first step is to install the Rutta Helm chart into your Kubernetes cluster.
         </p>
+        <code><pre>{
+`helm install https://github.com/logary/logary/tree/master/src/services/rutta-helm-chart && \\
+    --name rutta && \\
+    --namespace monitoring`
+        }</pre></code>
         <p>
-          <code>helm install https://github.com/logary/logary/tree/master/src/services/rutta-helm-chart --name rutta</code>
+          Or if you've downloaded the chart;
         </p>
+        <code><pre>{
+`helm install ./rutta-helm-chart && \\
+    --name rutta && \\
+    --namespace monitoring`
+        }</pre></code>
+        <p>
+          If you use a local values file, you can upgrade the chart.
+        </p>
+        <code><pre>{
+`helm upgrade --debug --install rutta && \\
+    ./rutta-helm-chart && \\
+    --namespace monitoring && \\
+    --values values/rutta.yaml`
+        }</pre></code>
       </DocSection>
       <DocSection {...toc[2]}>
         <h2 className="section-title">Rutta Router mode</h2>
+        <p>The router mode lets you take inputs from a `listener` (tcp, udp, ...), interpret it with a `codec` and then send it to a `target`.</p>
         <img src={rutta} alt="Rutta in Router mode" />
       </DocSection>
     </DocPage>
