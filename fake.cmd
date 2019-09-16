fd4b18ce9042bffe7d99189d47332e40ba07f0f4
Henrik Feldt <henrik@haf.se>
2019-09-16 23:15:43 +0200

    Try to fix AppVeyor build

--- fake.cmd
+++ fake.cmd
@@ -1,7 +1,10 @@
 SET TOOL_PATH=.fake
 
+
 IF NOT EXIST "%TOOL_PATH%\fake.exe" (
   dotnet tool install fake-cli --tool-path ./%TOOL_PATH%
 )
+
+dotnet tool install paket --tool-path .paket --version 5.219.0
 
 "%TOOL_PATH%/fake.exe" %*
