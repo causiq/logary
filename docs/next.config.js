5b1cef3c0fb339fc288ba3428879b9abbeac0e52
Henrik Feldt <henrik@haf.se>
2020-02-15 11:30:06 +0100

    target:serverless in docs/

--- docs/next.config.js
+++ docs/next.config.js
@@ -2,5 +2,5 @@ const withCSS = require('@zeit/next-css')
 const withSass = require('@zeit/next-sass')
 const withImages = require('next-images')
 module.exports = withCSS(withSass(withImages({
-  target: 'server'
+  target: 'serverless'
 })))
