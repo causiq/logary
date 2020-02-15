da3ffff774aa4a64779e1f57ce2b756f5b8779d2
Henrik Feldt <henrik@haf.se>
2020-02-15 11:55:46 +0100

    Update fake.cmd

--- fake.cmd
+++ fake.cmd
@@ -5,6 +5,6 @@ IF NOT EXIST "%TOOL_PATH%\fake.exe" (
   dotnet tool install fake-cli --tool-path ./%TOOL_PATH%
 )
 
-dotnet tool install paket --tool-path .paket --version 5.219.0
+dotnet tool install paket --tool-path .paket --version 5.236.0
 
 "%TOOL_PATH%/fake.exe" %*
