150da2e7fdd0bd2c871735f57a4a3c61567f44b8
Henrik Feldt <henrik@haf.se>
2020-05-22 15:00:55 +0200

    Add a bit of spacing above the iframe

--- docs/components/OutreachLayout.tsx
+++ docs/components/OutreachLayout.tsx
@@ -1,16 +1,11 @@
 import classNames from 'classnames'
 import Head from 'next/head'
-import { ReactNode } from 'react'
+import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react'
 
-type Props = Readonly<{
-  title: string;
-  className?: string | string[];
-  children?: ReactNode;
-  noChat?: boolean;
-}>
+type Props = Readonly<{ title: string; children?: ReactNode; }> & Partial<DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>>
 
 // Actual layout
-const Layout = ({ title, className = [], children, ...rest }: Props) => {
+const Layout = ({ title, className, children, ...rest }: Partial<Props>) => {
   return <div className={classNames(className, 'layout')} {...rest}>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" key="viewport" />
--- docs/pages/outreach/index.tsx
+++ docs/pages/outreach/index.tsx
@@ -3,17 +3,21 @@ import OutreachLayout from '../../components/OutreachLayout'
 
 export default function JSDocs() {
   return (
-    <OutreachLayout title='Heard of the OODA loop?'>
-      <h1>Head of the OODA loop?</h1>
-      <h2>Find out</h2>
-      <p>Most dev teams observe thier software really well. We want to do both observe and orient with you. We want to go beyond showing aggregated data, to show you correlations.</p>
-      <p>Collaborate with your Product Manager; we want to build logs, metrics and distributed tracing into a single product-metrics oriented product.</p>
-      <p>Pick a date for an intro; we're interested in your use-case, below you find my calendar slots for a 20 minute meeting.</p>
-      <Calendly />
+    <OutreachLayout title='Heard of the OODA loop?' style={{ backgroundColor: '#efffff'}}>
+      <main>
+        <h1>Know of the OODA loop?</h1>
+        <h2>Find out</h2>
+        <p>Most dev teams observe thier software really well. We want to do both observe and orient with you. We want to go beyond showing aggregated data, to show you correlations.</p>
+        <p>Collaborate with your Product Manager; we want to build logs, metrics and distributed tracing into a single product-metrics oriented product.</p>
+        <p>Pick a date for an intro; we're interested in your use-case, below you find my calendar slots for a 20 minute meeting.</p>
+        <Calendly />
+      </main>
       <style global jsx>{`
         body { background-color: #EFFFFF; }
         .layout { padding: 2em; }
+        main { max-width: 450px; margin: 0 auto;}
         h2 { margin-bottom: 2em; }
+        iframe { margin-top: 2em; }
         p {
           margin: 1.5em 0;
           font-size: 1.05rem;
