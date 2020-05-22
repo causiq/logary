4061f9f7291f712a6d6ed3e3bf5b497b0af36519
Henrik Feldt <henrik@haf.se>
2020-05-22 15:06:35 +0200

    More emojis FTW

--- docs/components/OutreachLayout.tsx
+++ docs/components/OutreachLayout.tsx
@@ -9,6 +9,12 @@ const Layout = ({ title, className, children, ...rest }: Partial<Props>) => {
   return <div className={classNames(className, 'layout')} {...rest}>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" key="viewport" />
+        <link rel="icon" type="image/png" href={require("../public/images/favicon-32x32.png")} sizes="32x32" key="favicon" />
+        <link rel="icon" type="image/png" href={require("../public/images/favicon-64x64.png")} sizes="64x64" key="favicon-64x64" />
+        <link rel="icon" type="image/png" href={require("../public/images/favicon-96x96.png")} sizes="96x96" key="favicon-96x96" />
+        <link rel="icon" type="image/png" href={require("../public/images/icon-200x200.png")} sizes="200x200" key="icon-200x200" />
+        <link rel="apple-touch-icon" sizes="200x200" href={require("../public/images/icon-200x200.png")} key="apple-touch-icon-200x200" />
+        <link rel="apple-touch-icon" sizes="500x500" href={require("../public/images/icon-500x500.png")} key="apple-touch-icon-500x500" />
       <meta charSet="utf-8" key="charset" />
       <title>{title}</title>
     </Head>
--- docs/pages/outreach/index.tsx
+++ docs/pages/outreach/index.tsx
@@ -3,13 +3,14 @@ import OutreachLayout from '../../components/OutreachLayout'
 
 export default function JSDocs() {
   return (
-    <OutreachLayout title='Heard of the OODA loop?' style={{ backgroundColor: '#efffff'}}>
+    <OutreachLayout title='Logary Analytics' style={{ backgroundColor: '#efffff'}}>
       <main>
-        <h1>Know of the OODA loop?</h1>
-        <h2>Find out</h2>
-        <p>Most dev teams observe thier software really well. We want to do both observe and orient with you. We want to go beyond showing aggregated data, to show you correlations.</p>
-        <p>Collaborate with your Product Manager; we want to build logs, metrics and distributed tracing into a single product-metrics oriented product.</p>
-        <p>Pick a date for an intro; we're interested in your use-case, below you find my calendar slots for a 20 minute meeting.</p>
+        <h1>Heard of the OODA loop?</h1>
+        <h2>Find out ✨</h2>
+        <p>Most dev teams have logs are are en-route to full observability 👀; this lets them do the "observe" bit of the OODA loop.</p>
+        <p>We want teams to be better at the "orient" 🧠 bit of the loop; to understand before deciding. We want to go beyond showing aggregated data.</p>
+        <p>Collaborate with your Product Manager (🥩); we want to build logs, metrics and distributed tracing into a single product-metrics oriented product.</p>
+        <p>Pick a date 🗓 for an intro; we're interested in your use-case, below you find my calendar slots for a 20 minute meeting.</p>
         <Calendly />
       </main>
       <style global jsx>{`
