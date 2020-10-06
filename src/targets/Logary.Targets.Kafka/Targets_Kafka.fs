e5db294b9174a0e92d06832a68c956f2d0dbc3d8
Henrik Feldt <henrik@haf.se>
2020-10-06 18:06:10 +0200

    Add default kafka port to default config for Kafka target

--- src/targets/Logary.Targets.Kafka/Targets_Kafka.fs
+++ src/targets/Logary.Targets.Kafka/Targets_Kafka.fs
@@ -67,9 +67,10 @@ let empty =
     replicationFactor = 2us
     batchSize = 100us
     prodConfFile = None
-    prodConf = ProducerConfig(BootstrapServers="ingress-broker-kafka-bootstrap.kafka.svc",
+    prodConf = ProducerConfig(BootstrapServers="ingress-broker-kafka-bootstrap.kafka.svc:9092",
                               EnableIdempotence=Nullable<_> true,
-                              MessageSendMaxRetries=Nullable Int32.MaxValue)
+                              MessageSendMaxRetries=Nullable Int32.MaxValue
+                              )
   }
 
 type KafkaTargetExn(message, error: Error) =
